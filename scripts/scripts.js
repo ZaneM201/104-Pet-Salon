8c4e60c57330c55ef6b709e80c32b175f7b67d5a
Zane Moon <zanem201@gmail.com>
2025-07-31 14:08:55 -0700

    functions 1-3 done, 4 started

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,6 @@
-let pets=[{name: "Milo", age: 4, gender: "Male", service: "The Works", breed: "Australian Shepard"}{name: "Maple", age: 4, gender: "Female", service: "Nail Trim", breed: "Greyhound Mix"}{name: "Tye", age: 16, service: "Nail Trim", breed:" Border Collie"}];
+console.log("Pet Salon");
+let pet=[{name: "Milo", age: 4, gender: "Male", service: "The Works", breed: "Australian Shepard"},{name: "Maple", age: 4, gender: "Female", service: "Nail Trim", breed: "Greyhound Mix"},{name: "Tye", age: 16, service: "Nail Trim", breed:" Border Collie"}];
+const maxPets = pet.length;
+const petNames = pet.map(pet => pet.name);
+document.getElementById("petSum").innerHTML = pet.length;
+document.getElementById("petNames").innerHTML = petNames;
