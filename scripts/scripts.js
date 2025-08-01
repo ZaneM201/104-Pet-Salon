d75eed7fb71f7fa44173bde82381dc0e087b44cf
Zane Moon <zanem201@gmail.com>
2025-07-31 22:58:47 -0700

    Broke it then fixed it

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,33 @@
 console.log("Pet Salon");
 let pet1={
-    name: "Milo";
-    age: 4;
-    gender: "Male";
-    service: "The works";
-    breed: "Australian Shepard"
+    name: "Milo", 
+    age: 4,
+    gender: "Male",
+    service: "The works",
+    breed: "Australian Shepard",
 };
 let pet2={
-    name: "Maple";
-    age: 4;
-    gender: "Female";
-    service: "Nail Trim";
-    breed: "Greayhound mix";
+    name: "Maple", 
+    age: 4,
+    gender: "Female",
+    service: "Nail Trim",
+    breed: "Greayhound mix",
 };
 let pet3={
-    name: "Tye";
-    age: 16;
-    gender: "Male";
-    service: "Nail Trim";
-    breed: "Border Colie mix";
+    name: "Tye", 
+    age: 16,
+    gender: "Male",
+    service: "Nail Trim",
+    breed: "Border Colie mix"
 };
-let pets=[pet1, pet2, pet3];
-for(let i=0: i<pets.length; i++){
-    document.getElementById("petNames").innerHTML += i;
-}
-const petNames = pet.map(pet => pet.name);
-document.getElementById("petSum").innerHTML = pet.length;
-
+let pets = [pet1, pet2, pet3];
+/* Names */
+const maxPets = pets.length;
+const petNames = pets.map(pets => pets.name);
+document.getElementById("petNames").innerHTML = petNames;
+/* Registered */
+document.getElementById("petSum").innerHTML = pets.length;
+/* Average Age */
 const petAge = pet.map(pet => pet.age);
 const sum = petAge.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 const averageAge = sum / petAge.length;
