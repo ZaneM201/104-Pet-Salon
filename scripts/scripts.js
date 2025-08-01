74e5a072e0484f2ae726f67a6cc5dc8bbc4d0510
Zane Moon <zanem201@gmail.com>
2025-07-31 22:13:29 -0700

    Fixing JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,26 @@
 console.log("Pet Salon");
-let pet=[{name: "Milo", age: 4, gender: "Male", service: "The Works", breed: "Australian Shepard"},{name: "Maple", age: 4, gender: "Female", service: "Nail Trim", breed: "Greyhound Mix"},{name: "Tye", age: 16, service: "Nail Trim", breed:" Border Collie"}];
+let pet1{
+    name: "Milo";
+    age: 4;
+    gender: "Male";
+    service: "The works";
+    breed: "Australian Shepard"
+};
+let pet2{
+    name: "Maple";
+    age: 4;
+    gender: "Female";
+    service: "Nail Trim";
+    breed: "Greayhound mix";
+};
+let pet3{
+    name: "Tye";
+    age: 16;
+    gender: "Male";
+    service: "Nail Trim";
+    breed: "Border Colie mix";
+};
+let pet=[pet1, pet2, pet3];
 const maxPets = pet.length;
 const petNames = pet.map(pet => pet.name);
 document.getElementById("petSum").innerHTML = pet.length;
