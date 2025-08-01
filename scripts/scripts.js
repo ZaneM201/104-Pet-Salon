abf08349f83fd90ca247105ad59da7fa88574a06
Zane Moon <zanem201@gmail.com>
2025-07-31 22:20:35 -0700

    Fixing JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,32 @@
 console.log("Pet Salon");
-let pet1{
+let pet1={
     name: "Milo";
     age: 4;
     gender: "Male";
     service: "The works";
     breed: "Australian Shepard"
 };
-let pet2{
+let pet2={
     name: "Maple";
     age: 4;
     gender: "Female";
     service: "Nail Trim";
     breed: "Greayhound mix";
 };
-let pet3{
+let pet3={
     name: "Tye";
     age: 16;
     gender: "Male";
     service: "Nail Trim";
     breed: "Border Colie mix";
 };
-let pet=[pet1, pet2, pet3];
-const maxPets = pet.length;
+let pets=[pet1, pet2, pet3];
+for(let i=0: i<pets.length; i++){
+    document.getElementById("petNames").innerHTML += i;
+}
 const petNames = pet.map(pet => pet.name);
 document.getElementById("petSum").innerHTML = pet.length;
-document.getElementById("petNames").innerHTML = petNames;
+
 const petAge = pet.map(pet => pet.age);
 const sum = petAge.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 const averageAge = sum / petAge.length;
