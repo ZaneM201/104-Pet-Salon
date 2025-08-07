f14b17f41813986e761c4e4417ccefba6edf9763
Zane Moon <zanem201@gmail.com>
2025-08-06 17:32:14 -0700

    cleaned up unnecessary comments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,18 +8,3 @@ let salon={
 document.getElementById("address").innerHTML = salon.address;
 document.getElementById("email").innerHTML = salon.email;
 document.getElementById("phone").innerHTML = salon.phone;
-
-/* Names 
-let petNamesHTML = "";
-for(i=0; i<pet.length; i++){
-    let pet = pet[i];
-    petNamesHTML += pet[i].name + "<br>";
-}*/
-//document.getElementById("registeredNames").innerHTML = petNamesHTML;
-/* Registered */
-//document.getElementById("petSum").innerHTML = pet.length;
-/* Average Age */
-//const petAge = pet.map(pet => pet.age);
-//const sum = petAge.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-//const averageAge = sum / petAge.length;
-//console.log("The average age is: ", averageAge);
