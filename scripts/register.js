bf61322b39f1334cf38e5f03ea9bf4a74d32c8ed
Zane Moon <zanem201@gmail.com>
2025-08-14 21:15:32 -0700

    dark mode started

--- scripts/register.js
+++ scripts/register.js
@@ -1,9 +1,9 @@
 const petRegistrationForm = document.getElementById("petRegistrationForm");
 
-let pet=[];
+let pets=[];
 
 function displayAllPets(){
-    pet.forEach(petObj =>{
+    pets.forEach(petObj =>{
         displayRow(petObj);
     });
 }
@@ -19,12 +19,12 @@ function Pet(name, age, gender, breed, service){
 const pet1=new Pet("Milo", 4, "Male", "Dog", "The Works");
 const pet2=new Pet("Maple", 4, "Female", "Dog", "Bath & Flee");
 const pet3=new Pet("Tye", 16, "Male", "Dog", "Nail Trim");
-pet.push(pet1);
-pet.push(pet2);
-pet.push(pet3);
+pets.push(pet1);
+pets.push(pet2);
+pets.push(pet3);
 
 displayAllPets();
-document.getElementById("petsRegistered").innerHTML = pet.length;
+document.getElementById("petsRegistered").innerHTML = pets.length;
 averageAge();
 
 function registerPet(event){
@@ -37,10 +37,10 @@ function registerPet(event){
     const service = petRegistrationForm.elements["petService"].value;
     
     const newPet = new Pet(name, age, gender, breed, service);
-    pet.push(newPet);
+    pets.push(newPet);
     displayRow(newPet);
     petRegistrationForm.reset();
-    document.getElementById("petsRegistered").innerHTML = pet.length;
+    document.getElementById("petsRegistered").innerHTML = pets.length;
 }
 
 function displayRow(petObj){
@@ -65,12 +65,12 @@ function displayRow(petObj){
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Delete";
     deleteBtn.addEventListener('click', function() {
-        const petIndex = pet.findIndex(p => p === petObj);
+        const petIndex = pets.findIndex(p => p === petObj);
         if (petIndex > -1) {
-            pet.splice(petIndex, 1);
+            pets.splice(petIndex, 1);
         }
         newRow.remove();
-        document.getElementById("petsRegistered").innerHTML = pet.length;
+        document.getElementById("petsRegistered").innerHTML = pets.length;
         averageAge();
     });
     action.appendChild(deleteBtn);
@@ -87,7 +87,7 @@ function displayRow(petObj){
 }
 /* average Age */
 function averageAge(){
-    const petAge = pet.map(pet => pet.age);
+    const petAge = pets.map(pets => pets.age);
     const sum = petAge.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
     const averageAge = sum / petAge.length;
     console.log(`The average age is: ${averageAge}`);
--- scripts/service.js
+++ scripts/service.js
@@ -1,3 +1,13 @@
+/* dark mode */
+$("#changeModeButton").click(function(){
+    $("body").toggleClass("dark-mode");
+    if($("body").hasClass("dark-mode")){
+        $("#changeModeButton").text("🌙");
+    }else{
+        $("#changeModeButton").text("☀️");
+    }
+});
+
 let services=[];
 const requiredService=document.getElementById("requiredService");
 const requiredDes=document.getElementById("requiredDes");
@@ -5,35 +15,45 @@ const requiredPrice=document.getElementById("requiredPrice");
 
 const serviceRegistrationForm=document.getElementById("serviceRegistrationForm");
 
+const service1=new Services("The Works", "Your furry friend desrves the best! Full spa experience to let your pet relax to the fullest.", 80.00);
+const service2=new Services("Bath & Flee", "Get your pet ready to fight the outdoors with our Bath and Flee treatment!", 25.00);
+const service3=new Services("Nail Trim", "Does your pet make to much noise when walking on your floors? Well not any more after a nail trim.", 10.00);
+
+services.push(service1);
+services.push(service2);
+services.push(service3);
+
+console.log(services);
+
 function Services(service, description, price){
-    this.service=service,
-    this.description=description,
-    this.price=price
+    this.service = service,
+    this.description = description,
+    this.price = price
 }
  
 function serviceRegistration(){
     event.preventDefault();
-    const service=serviceRegistrationForm.elements["requiredService"].value;
-    const description=serviceRegistrationForm.elements["requiredDes"].value;
-    const price=serviceRegistrationForm.elements["requiredPrice"].value;
+    const service = serviceRegistrationForm.elements["requiredService"].value;
+    const description = serviceRegistrationForm.elements["requiredDes"].value;
+    const price = serviceRegistrationForm.elements["requiredPrice"].value;
 
-    let isValid=true;
+    let isValid = true;
 
-    if(requiredService.value===""){
+    if(requiredService.value === ""){
         $("#requiredService").css("border", "solid 1px red");
         isValid=false;
     }else{
         $("#requiredService").css("border","solid 1px grey");
     }
 
-    if(requiredDes.value===""){
+    if(requiredDes.value === ""){
         $("#requiredDes").css("border", "solid 1px red");
         isValid=false;
     }else{
         $("#requiredDes").css("border","solid 1px grey");
     }
 
-    if(requiredPrice.value===""){
+    if(requiredPrice.value === ""){
     $("#requiredPrice").css("border", "solid 1px red");
     isValid=false;
     }else{
@@ -43,7 +63,7 @@ function serviceRegistration(){
     if(!isValid){
         return;
     }
-    const newService=new Services(service, description, price);
+    const newService = new Services(service, description, price);
     services.push(newService);
     console.log(services);
     serviceRegistrationForm.reset(); 
@@ -52,3 +72,41 @@ function serviceRegistration(){
 $("#serviceSubmit").click(function(){
     serviceRegistration();
 });
+
+/*$(document).ready(function (){
+
+    // The user clicked Register Service button -
+    // We will catch the click and manipulate the information
+
+    // Select the form by id using jQuery 
+    $("#serviceRegistrationForm").on("submit", function(e){
+        e.preventDefault();
+
+        // Get the values using jQuery
+        const serviceName = $("#requiredService").val().trim();
+        const serviceDescription = $("#requiredDes").val().trim();
+        const servicePrice = $("#requiredPrice").val().trim();
+
+        // Confirm we are retriving the values
+        console.log(`${serviceName}, ${serviceDescription}, ${servicePrice}`);
+
+        // Validate the values
+        if(!serviceName || !serviceDescription || servicePrice <=0){
+            // alert the user this is not valid data
+            $("#requiredService").css("border", "solid 1px red");
+            $("#requiredDes").css("border", "solid 1px red");
+            $("#requiredPrice").css("border", "solid 1px red");
+            
+        }else{
+            // Print/Save the information
+            alert("Service has been Registered");
+        }
+
+        // Notify the user
+
+        // Clear the form
+        $("#serviceRegistrationForm")[0].reset();
+    });
+
+});
+*/
